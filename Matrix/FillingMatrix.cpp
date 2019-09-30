d4c0248fc6efba738758861cbca76288097ef3db
buyalova-uliana <buyalova@yandex.ru>
2019-09-30 20:07:01 +0300

    add filling

--- Matrix/FillingMatrix.cpp
+++ Matrix/FillingMatrix.cpp
@@ -61,7 +61,7 @@ std::pair<Matrix, std::vector<double>> FillingData::FillingFromFile(char* filena
     }
 }
 
-std::pair<Matrix, std::vector<double>> FillingData::FillingWithFunc(size_t n)
+std::pair<Matrix, std::vector<double>> FillingData::FillingWithFunc_1(size_t n)
 {
 	Matrix A(n);
 	std::vector<double> b(n);
@@ -79,21 +79,123 @@ std::pair<Matrix, std::vector<double>> FillingData::FillingWithFunc(size_t n)
     return std::make_pair(A, b);
 }
 
+std::pair<Matrix, std::vector<double>> FillingData::FillingWithFunc_2(size_t n)
+{
+	Matrix A(n);
+	std::vector<double> b(n);
+	for (size_t i = 0; i < n; ++i)
+	{
+		for (size_t j = 0; j < n; ++j)
+		{
+			if (j == i)
+			{
+				if (i == 0)
+					A[i][j] = -1;
+				if (i == n - 1)
+					A[i][j] = -(n - 1) / n;
+				A[i][j] = -2;
+			}
+			if ((j == i + 1) || (j = i - 1))
+				A[i][j] = 1;
+			else
+				A[i][j] = 0;
+		}
+	}
+	for (size_t i = 0; i < n - 1; ++i)
+		b[i] = 0;
+	b[n - 1] = 1;
+	return std::make_pair(A, b);
+}
+//===================================----2----============================================
+std::pair<Matrix, std::vector<double>> FillingData::FillingWithFunc_1_1(size_t n)
+{
+	Matrix A(n);
+	std::vector<double> b(n);
+	for (size_t i = 0; i < n; ++i)
+	{
+		for (size_t j = 0; j < n; ++j)
+		{
+			A[i][j] = 1. / (i + j + 1);
+			if (j % 2 == 1)
+				b[i] += A[i][j];
+		}
+	}
+	return std::make_pair(A, b);
+}
+
+int FillingData::CheckForFunc_1_1(std::vector<double>& x)
+{
+	std::vector<double> s(x.size());
+	for (size_t i = 0; i < x.size(); ++i)
+	{
+		if (i % 2 == 0)
+			s[i] = 1;
+		else if (i % 2 == 1)
+			s[i] = 0;
+	}
+	for (size_t i = 0; i < s.size(); ++i)
+		s[i] -= x[i];
+	double sum = 0;
+	for (size_t i = 0; i < s.size(); ++i)
+		sum += s[i] * s[i];
+	return sqrt(sum);
+}
 
 
+///////////////////////////////////////////////////////////////////////////////////
+void FillingData::TriangularView(Matrix& matrix_, std::vector<double>& b_)
+{
+	int k = 0;
+	while (k < matrix_.n)
+	{
+		if (matrix_[k][k] == 0)
+		{
+			for (int i = k + 1; i < matrix_.size(); ++i)
+			{
+				if (matrix_[i][k] != 0)
+				{
+					MethodJordan::swap_row(matrix_, i, k);
+					MethodJordan::swap(b_[k], b_[i]);
+				}
+			}
+		}
+		if (matrix_[k][k] != 0)
+		{
+			double tmp = matrix_[k][k];
+			for (int i = k; i < matrix_[k].n; ++i)
+			{
+				matrix_[k][i] = matrix_[k][i] / tmp;
 
+			}
 
+			b_[k] = (b_[k] / tmp);
 
+			for (int i = k + 1; i < matrix_.size(); ++i)
+			{
+				double tmp = matrix_[i][k];
+				for (int j = k; j < matrix_[k].n; ++j)
+				{
+					matrix_[i][j] -= tmp * matrix_[k][j];
+				}
+				b_[i] -= tmp * b_[k];
+			}
+		}
+		++k;
+	}
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
+std::vector<double> FillingData::GaussMethod()
+{
+	Matrix matrix_ = ??;
+	std::vector<double> b_ = ??;
+	TriangularView(matrix_, b_);
+	std::vector<double> x(matrix.n);
+	for (int k = x.size() - 1; k >= 0; --k)
+	{
+		x[k] = b_[k];
+		for (int i = 0; i < k; ++i)
+			b_[i] = b_[i] - matrix_[i][k] * x[k];
+	}
+	return x;
+}
--- Matrix/FillingMatrix.h
+++ Matrix/FillingMatrix.h
@@ -11,5 +11,12 @@ class FillingData
 public:
     static std::pair<Matrix, std::vector<double>> FillingFromKeyboar();
     static std::pair<Matrix, std::vector<double>> FillingFromFile(char* filename);
-	static std::pair<Matrix, std::vector<double>> FillingWithFunc(size_t n);
+	static std::pair<Matrix, std::vector<double>> FillingWithFunc_1(size_t n);
+	static std::pair<Matrix, std::vector<double>> FillingWithFunc_2(size_t n);
+	static std::pair<Matrix, std::vector<double>> FillingWithFunc_1_1(size_t n);
+	static int CheckForFunc_1_1(std::vector<double>& x);
+
+	///////////////////////////////////////////////////////////////
+	void TriangularView(Matrix& matrix_, std::vector<double>& b_);
+	std::vector<double> GaussMethod();
 };
